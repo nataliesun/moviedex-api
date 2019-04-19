0a543ab8015473e0440c4ff964eb7151b51c3c87
nataliesun <natsun121@gmail.com>
2019-04-18 17:40:23 -0700

    added helmet, cors, and avg vote query

--- server.js
+++ server.js
@@ -1,16 +1,26 @@
+require('dotenv').config();
 const express = require('express')
 const morgan = require('morgan')
+const helmet = require('helmet');
 const cors = require('cors')
 const movieData = require('./movieData.json')
 
 const app = express()
 
 app.use(morgan('dev'))
-app.use(cors())
+app.use(cors());
+app.use(helmet());
 
-// app.use((req, res) => {
-  
-// })
+app.use(function validateBearerToken(req, res, next) {
+    const apiToken = process.env.API_TOKEN;
+    const authToken = req.get('Authorization');
+
+    if(!authToken || authToken.split(' ')[1] !== apiToken) {
+        return res.status(401).json({ error: 'Unauthorized request'});
+    }
+
+    next();
+})
 
 app.get('/movie', function handleGetMovie(req, res) {
     let response = movieData;
@@ -26,8 +36,13 @@ app.get('/movie', function handleGetMovie(req, res) {
             movie.country.toLowerCase().includes(req.query.country.toLowerCase())
         )
     }
+    
+    if (req.query.avg_vote) {
+        const value = parseFloat(req.query.avg_vote);
+        response = response.filter(movie => movie.avg_vote >= value
+        )
 
-
+    }
 
     res.json(response)
 })
@@ -39,5 +54,3 @@ app.listen(PORT, () => {
 })
 
 
-
-console.log('hello')
